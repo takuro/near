44fd3319da9c7d5bd1bcd235a0c7c4d64497f44e
Takuro Ishii <isitkr@gmail.com>
2011-07-03 14:03:11 +0900

    sendable message.

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ var app = module.exports = express.createServer();
 // mongodb
 mg.connect('mongodb://localhost/location');
 var Schema = mg.Schema;
+
 var location_schema = new Schema({ id: String, lat: String, lon: String, ch: String });
 var Location = mg.model('Location', location_schema);
-var loc = new Location();
 
 // distance
 // via : http://d.hatena.ne.jp/sato_shin/20061113/1163414997
@@ -34,8 +34,8 @@ function distance(from, to) {
   var to_x = to.x * Math.PI / 180;
   var to_y = to.y * Math.PI / 180;
   var deg = Math.sin(from_y) * Math.sin(to_y) + Math.cos(from_y) * Math.cos(to_y) * Math.cos(to_x-from_x);
-  var dist = 6378140 * (Math.atan( -deg / Math.sqrt(-deg * deg + 1)) + Math.PI / 2);
-  return Math.round(dist);
+  return 6378140 * (Math.atan( -deg / Math.sqrt(-deg * deg + 1)) + Math.PI / 2);
+  //return Math.round(dist);
 }
 
 // Configuration
@@ -66,6 +66,10 @@ app.get('/', function(req, res){
 });
 
 app.post('/geo', function(req, res){
+  var location_schema = new Schema({ id: String, lat: String, lon: String, ch: String });
+  var Location = mg.model('Location', location_schema);
+  var loc = new Location();
+
   if (req.body.lat && req.body.lon) {
     var lat = "" + req.body.lat;
     var lon = "" + req.body.lon;
@@ -78,30 +82,34 @@ app.post('/geo', function(req, res){
 
     if (key != "") {
       var d = config.near_distance;
-      var ch = '';
+      var ch = null;
       Location.find({}, function(err, docs) {
         if(!err) {
           for (var i = 0; i < docs.length; i++ ) {
             d = distance({x: lat, y: lon}, {x: docs[i].lat, y: docs[i].lon})
+            //console.log(d);
+
             if (parseInt(d) < config.near_distance) {
+              console.log("find near user : " + docs[i].ch);
               ch = docs[i].ch;
             }
           }
         } else {
           console.log("no data");
         }
-      });
 
-      loc.id = key,
-      loc.lat = lat,
-      loc.lon = lon;
-      if (ch) {
-        loc.ch = ch;
-      } else {
+        loc.id = key,
+        loc.lat = lat,
+        loc.lon = lon,
         loc.ch = key;
-      }
-      loc.save(function(err) { if(!err) console.log(loc) });
-
+        if (ch !== null) {
+          console.log("find near user");
+          loc.ch = ch;
+        }
+        loc.save(function(err) {
+          if(!err) console.log(loc)
+        });
+      });
     }
   }
   return false;
@@ -130,21 +138,26 @@ socket.on('connection', function(client) {
       Location.find({id: ""+client.sessionId}, function(err, doc) {
         if (typeof doc[0] != 'undefined') {
           client.joinChannel(doc[0].ch);
-          client.broadcastToChannel(data, doc[0].ch, client.sessionId);
         }
+        //client.broadcastToChannel(data, doc[0].ch, client.sessionId);
+        client.broadcastToChannel(data, doc[0].ch, client.sessionId);
       });
 
       // for you
+      /*
       speaker = '<header class="speaker">' + config.my_name + '</header></article>';
       msg = '<article class="bubble you"><div class="bubble"><p>' + obj.message + '</p></div>';
       data = json({name: speaker, message: msg});
-      client.send(data);
+      client.sendToMe(data, client.sessionId);
+      */
     }
   });
   client.on('disconnect', function() {
     // disconnect
-    //st_rm(client.sessionId);
-    //console.log('disconnect : ' + client.sessionId);
+    Location.find({id: ""+client.sessionId}, function (err, doc) {
+      if (!err && doc[0]) { doc[0].remove(); }
+    });
+    console.log('disconnect : ' + client.sessionId);
   });
 });
 
@@ -154,12 +167,23 @@ var Client  = require('socket.io/lib/socket.io/client.js');
 
 Client.prototype.broadcastToChannel = function(message, channel, myId){
   for (var i = 0, k = Object.keys(this.listener.clients), l = k.length; i < l; i++){
-    if(this.listener.clients[k[i]].channel == channel && this.listener.clients[k[i]].sessionId != myId) {
+    if(this.listener.clients[k[i]].channel == channel) {
+    //if(this.listener.clients[k[i]].channel == channel && this.listener.clients[k[i]].sessionId != myId) {
       this.listener.clients[k[i]].send(message);
     }
   }
   return this;
 };
+
+Client.prototype.sendToMe = function(message, myId){
+  for (var i = 0, k = Object.keys(this.listener.clients), l = k.length; i < l; i++){
+    if(this.listener.clients[k[i]].sessionId == myId) {
+      this.listener.clients[k[i]].send(message);
+    }
+  }
+  return this;
+};
+
 // クライアントがチャンネルに入るためのメソッド
 Client.prototype.joinChannel = function(channel){
   this.channel = channel;
